ce2071183730daa7beac0580c0b4b17df37ff790
Manpreet <manpreet123singh987@gmail.com>
2025-10-18 01:28:55 -0400

    Update server.js - remove email verification on startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -90,7 +90,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(async () => {
   // Verify email configuration
-  await verifyEmailConfig();
+
   
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
